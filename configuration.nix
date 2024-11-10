b936f48b8d8cc509933721737ff6cc9afababe63
Anwer Khan <deepwatrcreatur@gmail.com>
2024-11-10 11:19:29 -0500

    Initial commit, has working Nvidia driver and many useful packages

--- configuration.nix
+++ configuration.nix
@@ -134,6 +134,7 @@
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   git
+  gitAndTools.gh
   wget
   curl
   pciutils
