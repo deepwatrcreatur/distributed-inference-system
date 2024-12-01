9781c6bd360bde78edb50e785a2afab3ed241968
Anwer Khan <deepwatrcreatur@gmail.com>
2024-12-01 13:48:34 -0500

    Added some nix packages: Tigerbeetle and iperf3

--- configuration.nix
+++ configuration.nix
@@ -17,12 +17,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "inference1"; # Define your hostname.
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -80,35 +75,12 @@
     variant = "";
   };
 
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.deepwatrcreatur = {
     isNormalUser = true;
     description = "Anwer Khan";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
     ];
   };
 
@@ -140,6 +112,10 @@
   pciutils
   nvtop
   tailscale
+  elixir
+  erlang
+  tigerbeetle
+  iperf3
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -150,15 +126,9 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  #services.ollama = {
-  #  enable = true;
-    #acceleration = "cuda";
-  #};
-  #(pkgs.ollama.override { acceleration = "cuda"; })
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ 22 ];
