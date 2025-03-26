9fc42bf6135173b353304e53d9af167f663caff5
Anwer Khan <deepwatrcreatur@gmail.com>
2025-03-26 01:23:40 -0400

    added ghostty to get terminfo

--- configuration.nix
+++ configuration.nix
@@ -111,6 +111,7 @@
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   neovim
+  ghostty
   kitty
   nushell
   git
