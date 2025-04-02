579c46050515b8da64abaf5949ab527a4cedb895
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-02 07:40:18 -0400

    added bat and fzf

--- configuration.nix
+++ configuration.nix
@@ -183,9 +183,12 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     neovim
+    helix
     ghostty
     kitty
     nushell
+    bat
+    fzf
     netdata
     htop
     btop
