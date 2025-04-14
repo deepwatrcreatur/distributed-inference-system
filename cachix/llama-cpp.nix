b0e6d9420fed44d82614f503f3b798dc526b40b1
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-14 00:03:08 -0400

    added binary caches

---
+++ cachix/llama-cpp.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://llama-cpp.cachix.org"
+      ];
+      trusted-public-keys = [
+        "llama-cpp.cachix.org-1:H75X+w83wUKTIPSO1KWy9ADUrzThyGs8P5tmAbkWhQc="
+      ];
+    };
+  };
+}
